5e1584845cba33dd4df79088ed98fc9653407818
TranTam <tamvaa1306@gmail.com>
2022-09-25 12:43:50 +0700

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
